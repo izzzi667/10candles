bbbbb251997e50c53ab72dae3701ff80f0693e4c
Aleksey Evdokimov <izzzi667@gmail.com>
2021-12-12 21:47:47 +0300

    Add candle life time

--- src/Components/Candle.jsx
+++ src/Components/Candle.jsx
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import active from '../Static/candle.gif';
 import inactive from '../logo.svg';
+
+const MIN_CANDLE_LIFE_TIME = 1;
+const MAX_CANDLE_LIFE_TIME = 10;
 
 
 const Candle = (props) =>
@@ -9,6 +12,11 @@ const Candle = (props) =>
     const changeStatus = (props) => {
         setStatus(false);
     }
+    useEffect(() => {
+        const candleLife = MIN_CANDLE_LIFE_TIME+Math.floor(Math.random() * (MAX_CANDLE_LIFE_TIME-MIN_CANDLE_LIFE_TIME)); 
+        setTimeout(changeStatus, 1000*candleLife);
+        
+    }, [])
 
     
     return <img 
@@ -17,5 +25,6 @@ const Candle = (props) =>
                 onClick={changeStatus}
             />
 }
+
 
 export default Candle;
