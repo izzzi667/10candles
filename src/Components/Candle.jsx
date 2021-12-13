2738b45e941408a9af0b8966db44046dd228cb4d
Aleksey Evdokimov <izzzi667@gmail.com>
2021-12-13 23:06:03 +0300

    Set timer data for real game

--- src/Components/Candle.jsx
+++ src/Components/Candle.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import active from '../Static/candle.gif';
 import inactive from '../logo.svg';
 
-const MIN_CANDLE_LIFE_TIME = 1;
-const MAX_CANDLE_LIFE_TIME = 10;
+const MIN_CANDLE_LIFE_TIME = 50;
+const MAX_CANDLE_LIFE_TIME = 150;
 
 
 const Candle = (props) =>
@@ -14,7 +14,7 @@ const Candle = (props) =>
     }
     useEffect(() => {
         const candleLife = MIN_CANDLE_LIFE_TIME+Math.floor(Math.random() * (MAX_CANDLE_LIFE_TIME-MIN_CANDLE_LIFE_TIME)); 
-        setTimeout(changeStatus, 1000*candleLife);
+        setTimeout(changeStatus, 60*1000*candleLife);
         
     }, [])
 
